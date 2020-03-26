517b41ff96e34fa0bbd4d19a34257370afb68726
Jeremy Moyers <jmoyers14@gmail.com>
2020-03-25 17:32:04 -0700

    global styles

--- src/Mixins.js
+++ src/Mixins.js
@@ -4,4 +4,13 @@ export const contentMixin = css`
   max-width: 1220px;
   margin: 0 auto;
   padding: 0 30px;
-`;
+`
+export const wrapper = css`
+  position: relative;
+  margin: auto;
+  max-width: 1680px;
+  overflow: hidden;
+  background-color: #fff;
+  z-index: 0;
+  box-shadow: 0 0.0625rem 0.25rem 0 rgba(61, 66, 80, 0.18);
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -14,10 +14,15 @@ import Footer from './Footer'
 import Header from "./header"
 import "./layout.css"
 import * as Mixins from '../Mixins'
+import GlobalStyles from '../GlobalStyles'
 
 export const Content = styled.div`
   ${Mixins.contentMixin}
-`;
+`
+
+export const LayoutWrapper = styled.div`
+  position: relative;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -32,17 +37,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyles/>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <Footer/>
-      </div>
+      <LayoutWrapper>{children}</LayoutWrapper>
+      <Footer/>
     </>
   )
 }
