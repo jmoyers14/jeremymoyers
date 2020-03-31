d7d8d84e4715398678fa196961487e2806f6adb7
Jeremy Moyers <jmoyers14@gmail.com>
2020-03-30 19:53:32 -0700

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jeremy Moyers`,
+    description: `Jeremy Moyer's personal website.`,
+    author: `jeremy moyers`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
